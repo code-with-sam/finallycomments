b4e27c550cdd88edbb5d94f332d8368038890994
Sam Billingham <sambillie.sb@gmail.com>
2018-02-04 22:52:04 +0000

    add config and env vars

---
+++ app/config.js
@@ -0,0 +1,11 @@
+let config = {
+    auth: {
+        client_id: process.env.CLIENT_ID,
+        redirect_uri: process.env.REDIRECT_URI,
+    },
+    session: {
+        secret: process.env.SESSION_SECRET
+    }
+};
+
+module.exports = config;
