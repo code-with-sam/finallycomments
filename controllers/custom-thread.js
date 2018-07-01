4e74e467e98ef234796e8e90bf4f9c0cf10f3936
Sam Billingham <sambillie.sb@gmail.com>
2018-07-01 17:05:26 +0100

    update - restructure custom thread

--- controllers/custom-thread.js
+++ controllers/custom-thread.js
@@ -1,6 +1,26 @@
 const Thread = require('../models/thread')
 const Domain = require('../models/domain')
+const { steem, getAccessFromRefresh } = require('../modules/steemconnect')
 const { URL } = require('url');
+const FINALLY_AUTHOR = 'finallycomments'
+const FINALLY_PERMLINK = 'finally-comments-thread'
+
+async function createThread(username) {
+  let newToken;
+  try { newToken = await getAccessFromRefresh(username) }
+  catch(error){console.log(error)}
+  steem.setAccessToken(newToken.access_token);
+  let permlink = `finally-${slug}`
+  let title = `${username}: Finally Thread`
+  let body = `${username} : This comment is a thread for the Finally Comments System. Visit https://finallycomments.com for more info.`
+  steem.comment(FINALLY_AUTHOR, FINALLY_PERMLINK, username, permlink, title, body, { app: 'finally.app' }, async (err, steemResponse) => {
+    if (err) { console.log(err); res.redirect('/404');
+    } else {
+      await Thread.insert({ author: author, slug: permlink, title: title })
+      res.redirect(`/thread/finallycomments/@${username}/${permlink}`)
+    }
+  });
+}
 
 module.exports.checkAndGenerate = async (req, res) => {
   let username = req.params.username
@@ -14,27 +34,8 @@ module.exports.checkAndGenerate = async (req, res) => {
     if(req.headers.referer === undefined) res.redirect('/404')
     let referer = new URL(req.headers.referer)
     let domains = await Domain.findOne(username)
-    if (domains.list.indexOf(referer.host) > -1 ){
-      let newToken
-      try { newToken = await getAccessFromRefresh(username) }
-      catch(error){console.log(error)}
-      steem.setAccessToken(newToken.access_token);
-      const FINALLY_AUTHOR = 'finallycomments'
-      const FINALLY_PERMLINK = 'finally-comments-thread'
-      let author = username
-      let permlink = `finally-${slug}`
-      let title = `${username}: Finally Thread`
-      let body = `${username} : This comment is a thread for the Finally Comments System. Visit https://finallycomments.com for more info.`
-      let parentAuthor = FINALLY_AUTHOR
-      let parentPermlink = FINALLY_PERMLINK
-      steem.comment(parentAuthor, parentPermlink, author, permlink, title, body, { app: 'finally.app' }, async (err, steemResponse) => {
-        if (err) { console.log(err); res.redirect('/404');
-        } else {
-          let newThread = { author: author, slug: permlink, title: title }
-          let response = await Thread.insert(newThread)
-          res.redirect(`/thread/finallycomments/@${username}/${permlink}`)
-        }
-      });
+    if (domains.list.indexOf(referer.host) > -1 ) {
+      createThread(username)
     } else { res.redirect('/404') }
   }
 }
